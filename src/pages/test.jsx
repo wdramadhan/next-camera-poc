7c22063789f0a6d47eec8ce511c0bb66dcccd78c
wdramadhan <wahyu.ramadhan@tiket.com>
2023-10-19 10:51:42 +0700

    add scanner page

---
+++ src/pages/test.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { QrReader } from "react-qr-reader";
+
+const Test = (props) => {
+  const [data, setData] = useState("No result");
+
+  return (
+    <div style={{ height: "100vh" }}>
+      <QrReader
+        constraints={{
+          facingMode: "environment",
+          aspectRatio: 9 / 16,
+        }}
+        onResult={(result, error) => {
+          if (!!result) {
+            setData(result?.text);
+          }
+
+          if (!!error) {
+            console.info(error);
+          }
+        }}
+        videoContainerStyle={{
+          background: "blue",
+          height: "700px",
+        }}
+      />
+      <p>{data}</p>
+    </div>
+  );
+};
+
+export default Test;
